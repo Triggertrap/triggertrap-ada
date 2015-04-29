3bd2afc3e8f480572eae5c458a021edfb825bf5a
Rich Harley <rich@triggertrap.com>
2015-04-29 09:03:09 +0100

    Missing Files
    Added Highspeedflash.h, Inputs.h, LongExposureHDR.h and Timelapse.h.
    
    Commented out Clock.h in wiring.c

---
+++ Triggertrap Ada/TriggerTrap/LongExposureHDR.h
@@ -0,0 +1,6 @@
+#ifndef LongExposureHDR_h
+#define LongExposureHDR_h
+
+char LongExposure_HDR(char input);
+
+#endif //LongExposureHDR_h
---
+++ Triggertrap Ada/TriggerTrap/Timelapse.h
@@ -0,0 +1,8 @@
+#ifndef TIMELAPSE_H
+#define TIMELAPSE_H
+
+
+char TimelapseRun(char input);
+char TimelapseInterval(char input);
+
+#endif //TIMELAPSE_H
